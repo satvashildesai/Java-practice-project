fb51b4397a47a41d9fd1e0fbe76fbeb4ef48ec1c
satvashildesai <satvashildesai01@gmail.com>
2023-11-29 16:52:32 +0530

    Add new Question-answer game and replace all the use Logger throught the project

--- src/main/java/com/practiceproject/game/App.java
+++ src/main/java/com/practiceproject/game/App.java
@@ -3,50 +3,54 @@ package com.practiceproject.game;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.LogManager;
 
-public class App 
-{
+public class App {
 	static BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main( String[] args )
-    {    	
-    	boolean isTerminate = false;
-    	
-    	try {
-    		do {
-            	System.out.println("\n\nWhich game you want to play?");
-            	System.out.println("1) Number game");
-            	System.out.println("2) Questions Answers game");
-            	System.out.print("3) EXIT \n==> ");
-            	
-            	// Take game choice from user            	
-            	String userChoice = bReader.readLine();
-            	
-            	switch (userChoice) {
-            		// Number Game            	
-    				case "1":
-    					NumberGame.startGame();	
-    					break;
-    					
-    				// Question Answer game    					
-    				case "2":
-    					
-    					break;
-    					
-    				// Exit from game    				
-    				case "3":
-    					isTerminate = true;
-    					System.out.println("Thank you!");
-    					break;
+	static Logger log = (Logger) LogManager.getLogger(App.class);
 
-    				default:
-    					System.out.println("Wrong input!");
-    					break;
-    			}
-        	}while(!isTerminate);
-		} 
-    	catch (IOException e) {e.printStackTrace();}
-    	catch (Exception e) {e.printStackTrace();}
-    	
-    }
+	public static void main(String[] args) {
+
+		boolean isTerminate = false;
+
+		try {
+			do {
+				log.info("\n\nWhich game you want to play?");
+				log.info("1) Number game");
+				log.info("2) Questions Answers game");
+				log.info("3) EXIT \n==> ");
+
+				// Take game choice from user
+				String userChoice = bReader.readLine();
+
+				switch (userChoice) {
+				// Number Game
+				case "1":
+					NumberGame.startGame();
+					break;
+
+				// Question Answer game
+				case "2":
+					QuestionAnswerGame.startGame();
+					break;
+
+				// Exit from game
+				case "3":
+					isTerminate = true;
+					log.info("Thank you!");
+					break;
+
+				default:
+					log.info("Wrong input!");
+					break;
+				}
+			} while (!isTerminate);
+		} catch (IOException e) {
+			log.error("Something wrong with I/O system!");
+		} catch (Exception e) {
+			log.error("Unwanted exception arise");
+		}
+	}
 }
